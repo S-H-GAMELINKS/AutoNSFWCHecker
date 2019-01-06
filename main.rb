d7cda757285e636e61b7fb1cd891e559b1f644a4
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-01-06 23:50:29 +0900

    add Exception handling

--- main.rb
+++ main.rb
@@ -14,17 +14,23 @@ vision = Google::Cloud::Vision.new project: env["project_id"].to_s
 stream = Mastodon::Streaming::Client.new(base_url: ENV["MASTODON_URL"], bearer_token: ENV["MASTODON_ACCESS_TOKEN"])
 client = Mastodon::REST::Client.new(base_url: ENV["MASTODON_URL"], bearer_token: ENV["MASTODON_ACCESS_TOKEN"])
 
-# Streaming for Local TimeLine
-stream.stream('public/local') do |toot|
-    toot.media_attachments.each do |media|
-        response = vision.image(media.url).safe_search
-        puts "Check!"
-        if response.adult? 
-            client.create_status("@#{toot.account.acct}@#{ENV["MASTODON_DOMAIN"]} えっちい画像なのでNSFWをつけてください……")
-        elsif response.violence?
-            client.create_status("@#{toot.account.acct}@#{ENV["MASTODON_DOMAIN"]} 暴力的な画像なのでNSFWをつけてください……")
-        elsif response.medical?
-            client.create_status("@#{toot.account.acct}@#{ENV["MASTODON_DOMAIN"]} 医療系な画像なのでNSFWをつけてください……")
+while true do
+    begin
+        # Streaming for Local TimeLine
+        stream.stream('public/local') do |toot|
+            toot.media_attachments.each do |media|
+                response = vision.image(media.url).safe_search
+                puts "Check!"
+                if response.adult? 
+                    client.create_status("@#{toot.account.acct}@#{ENV["MASTODON_DOMAIN"]} えっちい画像なのでNSFWをつけてください……")
+                elsif response.violence?
+                    client.create_status("@#{toot.account.acct}@#{ENV["MASTODON_DOMAIN"]} 暴力的な画像なのでNSFWをつけてください……")
+                elsif response.medical?
+                    client.create_status("@#{toot.account.acct}@#{ENV["MASTODON_DOMAIN"]} 医療系な画像なのでNSFWをつけてください……")
+                end
+            end
         end
+    rescue => error
+        puts error
     end
 end
